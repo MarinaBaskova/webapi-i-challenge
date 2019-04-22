f130f6a175a09192bffb55d80c0ea1fe86ff8c74
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-04-22 13:42:40 -0700

    added get req for all users and get by id req

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ server.get('/', (req, res) => {
 	res.send('Welcome to the App');
 });
 
+// GET Users
+
+server.get('/users', (req, res) => {
+	db
+		.find()
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err, message: 'The users information could not be retrieved.' });
+		});
+});
+
 server.listen(8000, () => {
 	console.log('Server is listening on port 8000');
+});
+
+// GET User by ID
+
+server.get('/users/:id', (req, res) => {
+	db
+		.findById(req.params.id)
+		.then((user) => {
+			if (user.length === 0) {
+				res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+			}
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The user information could not be retrieved.' });
+		});
 });
