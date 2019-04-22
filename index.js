4ea82e1fe7702dee0b988be01d047ba7a35851b5
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-04-22 13:14:14 -0700

    initial commit app setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send('Welcome to the App');
+});
+
+server.listen(8000, () => {
+	console.log('Server is listening on port 8000');
+});
